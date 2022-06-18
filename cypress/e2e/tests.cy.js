b9974b1bbe51c6265c78e11b679f6faaf02d5c59
karolmacheta <karol.macheta@gmail.com>
2022-06-18 17:41:27 +0200

    wip

--- cypress/e2e/tests.cy.js
+++ cypress/e2e/tests.cy.js
@@ -1,3 +1,5 @@
+import { mainPage } from "../../locators/mainPage.js"
+
 describe('QA Tasks', () => {
   it('checks the swapi for Darth Vader name in people/4', () => {
     cy.request('GET', 'https://swapi.dev/api/people/4/').then(
@@ -9,9 +11,9 @@ describe('QA Tasks', () => {
 
   it('checks /careers for one QA entry', () => {
     cy.visit('https://10clouds.com/');
-    cy.get('.nav-link-container [href="/careers/"] span').should('be.visible');
-    cy.get('.nav-link-container [href="/careers/"] span').click(); 
-    cy.url().should('eq','https://10clouds.com/careers/');
+    cy.get('.nav-link-container [href="/careers/"] span').should('be.visible').click();
+    //cy.get('.nav-link-container [href="/careers/"] span').click(); 
+    cy.url().should('eq', 'https://10clouds.com/careers/');
     cy.get('.job-offers__header-title').should('be.visible');
     cy.get('.job-offer__title').contains('QA Automation Engineer').should('have.length', 1);
   });
---
+++ cypress/locators/mainPage.js
@@ -0,0 +1,5 @@
+export const mainPage = {
+    careersBtn: '.nav-link-container [href="/careers/"] span',
+    careersPageHeader: '.job-offers__header-title',
+    jobTitle: '.job-offer__title',
+}
