4acfe6b7ceb9952b9ce4cf05405dfd53ad581b64
karolmacheta <karol.macheta@gmail.com>
2022-06-18 17:55:19 +0200

    done

--- cypress/e2e/tests.cy.js
+++ cypress/e2e/tests.cy.js
@@ -1,20 +1,19 @@
 import { mainPage } from "../locators/mainPage.js"
 
 describe('QA Tasks', () => {
+    it('checks /careers for one QA entry', () => {
+    cy.visit('https://10clouds.com/');
+    cy.get(mainPage.careersBtn).should('be.visible').click({force: true});
+    cy.url().should('eq', 'https://10clouds.com/careers/');
+    cy.get(mainPage.careersPageHeader).should('be.visible');
+    cy.get(mainPage.jobTitle).contains('QA Automation Engineer').should('have.length', 1);
+  });
+
   it('checks the swapi for Darth Vader name in people/4', () => {
     cy.request('GET', 'https://swapi.dev/api/people/4/').then(
       (response) => {
         expect(response.body).to.have.property('name', 'Darth Vader');
       }
     )
-  });
-
-  it('checks /careers for one QA entry', () => {
-    cy.visit('https://10clouds.com/');
-    cy.get(mainPage.careersBtn).should('be.visible').click({force: true});
-    //cy.get('.nav-link-container [href="/careers/"] span').click(); 
-    cy.url().should('eq', 'https://10clouds.com/careers/');
-    cy.get(mainPage.careersPageHeader).should('be.visible');
-    cy.get(mainPage.jobTitle).contains('QA Automation Engineer').should('have.length', 1);
   });
 });
