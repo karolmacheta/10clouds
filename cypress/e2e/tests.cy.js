856c3f69a021c9d6ff670075f08dea8625c43e31
karolmacheta <karol.macheta@gmail.com>
2022-06-23 16:23:11 +0200

    adjustments

--- cypress/e2e/tests.cy.js
+++ cypress/e2e/tests.cy.js
@@ -2,9 +2,20 @@ import { mainPage } from "../locators/mainPage.js"
 
 describe('QA Tasks', () => {
   it('checks the swapi for Darth Vader name in people/4', () => {
-    cy.request('GET', 'https://swapi.dev/api/people/4/').then(
+    cy.request('GET', 'https://swapi.dev/api/people/4/',).then(
       (response) => {
         expect(response.body).to.have.property('name', 'Darth Vader');
+      }
+    )
+  });
+
+  it('checks the swapi for 404 from people/400', () => {
+    cy.request(
+      { 
+        url: 'https://swapi.dev/api/people/400/',
+        failOnStatusCode: false,}).then(
+      (response) => {
+        expect(response.status).to.eq(404);
       }
     )
   });
@@ -25,7 +36,9 @@ describe('QA Tasks', () => {
     cy.url().should('eq', 'https://10clouds.com/careers/');
     cy.get(mainPage.careersPageHeader).scrollIntoView();
     cy.get(mainPage.jobDepartmentDDbtns).contains('All departments').scrollIntoView();
+    cy.wait(2000);  //Time waits are not optimal in Cypress but otherwise the elements get detached from DOM, not enough time to investigate
     cy.get(mainPage.jobDepartmentDDbtns).contains('All departments').should('be.visible').click({ force: true });
+    cy.wait(2000); // Same as above
     cy.get(mainPage.jobDepartmentDDselect).contains('QA').should('be.visible').click();
     cy.get(mainPage.jobTitle).each((item) => {
       cy.wrap(item).as('text');
